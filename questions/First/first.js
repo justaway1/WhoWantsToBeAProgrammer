c56f2ff2759ea147472016f4f124a02e42fba535
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-04-06 21:38:36 +0300

    Added Two Questions/ need hints to be fixed

--- questions/First/first.js
+++ questions/First/first.js
@@ -1,73 +1,136 @@
 const questionOneAnswers = {
-    'id-a': 'Stan',
-    'id-b': 'Brittany',
-    'id-c': 'Steven',
-    'id-d': 'Lebron'
+    'id-a': '120',
+    'id-b': '12',
+    'id-c': '30',
+    'id-d': '72'
 };
 
-
-const oneHundred = document.getElementById('oneHundred');
 
 const answerOneA = document.getElementById('question-one-id-a');
 const answerOneB = document.getElementById('question-one-id-b');
 const answerOneC = document.getElementById('question-one-id-c');
 const answerOneD = document.getElementById('question-one-id-d');
-const showImageMe = document.getElementById('showImage');
-const thequestion = document.getElementById('question-one');
+const showcash = document.getElementById('theCash');
+const sideMoney = document.getElementById('firstSum');
+const choiceA = document.querySelector('.choice-a');
+const choiceB = document.querySelector('.choice-b');
+const choiceC = document.querySelector('.choice-c');
+const choiceD = document.querySelector('.choice-d');
+const answerA = document.getElementById('answer-a')
+const answerB = document.getElementById('answer-b')
+const answerC = document.getElementById('answer-c')
+const answerD = document.getElementById('answer-d')
+const removetheAnswer = document.getElementById('removeAnswer');
+const removedAnswer = true;
 
 
 
 
 
-answerOneA.addEventListener('click', () => {
-    if (questionOneAnswers["id-a"] === 'Stan') {
+answerOneA.addEventListener('click', answerOne);
+answerOneB.addEventListener('click', answerTwo);
+answerOneC.addEventListener('click', answerThree);
+answerOneD.addEventListener('click', answerFour);
+removetheAnswer.addEventListener('click', removeAnswer);
+
+function answerOne() {
+
+    if (questionOneAnswers["id-a"] === '120') {
+        answerOneB.removeEventListener('click', answerTwo);
+        answerOneC.removeEventListener('click', answerThree);
+        answerOneD.removeEventListener('click', answerFour);
+        removetheAnswer.removeEventListener('click', removeAnswer);
         answerOneA.style.background = 'green';
-        oneHundred.style.backgroundColor = 'red';
+        sideMoney.setAttribute('id', 'sidemoney');
+    }
+    setTimeout(function () {
+        showcash.setAttribute('id', 'showingMoney');
+        showcash.innerHTML = '100$';
+
+    });
+
+    setTimeout(function () {
+        location.href = '../Second/secondQuestion.html';
+    }, 2000);
+};
+
+
+
+function answerTwo() {
+
+    if (questionOneAnswers["id-b"] === '12') {
+        answerOneA.removeEventListener('click', answerOne);
+        answerOneC.removeEventListener('click', answerThree);
+        answerOneD.removeEventListener('click', answerFour);
+        removetheAnswer.removeEventListener('click', removeAnswer);
+        answerOneB.style.background = 'red';
+        choiceB.style.color = 'white';
     }
 
     setTimeout(function () {
-        thequestion.style.display = 'none';
-        let img = document.createElement('img');
-        img.src = './images/ruby.png';
-        showImageMe.appendChild(img);
-        let input = document.createElement('input');
-        input.type = 'button';
-        input.value = 'Next Question';
-        document.getElementById('showImage').appendChild(input);
-        input.onclick = () => {
-            location.href = 'second.html';
-        }
+        answerOneA.style.backgroundColor = 'green';
+    }, 1000)
 
-    }, 2000);
+    tryAgain();
+}
 
-
-})
-answerOneB.addEventListener('click', () => {
-    if (questionOneAnswers["id-b"] === 'Brittany') {
-        answerOneB.style.background = 'red';
-        setInterval(() => {
-            answerOneA.style.background = 'green';
-
-        }, 1500);
-    }
-})
-answerOneC.addEventListener('click', () => {
-    if (questionOneAnswers["id-c"] === 'Steven') {
+function answerThree() {
+    if (questionOneAnswers["id-c"] === '30') {
+        answerOneA.removeEventListener('click', answerOne);
+        answerOneB.removeEventListener('click', answerTwo);
+        answerOneD.removeEventListener('click', answerFour);
+        removetheAnswer.removeEventListener('click', removeAnswer);
         answerOneC.style.background = 'red';
-        setInterval(() => {
-            answerOneA.style.background = 'green';
-
-        }, 1500);
+        choiceC.style.color = 'white';
     }
-})
-answerOneD.addEventListener('click', () => {
-    if (questionOneAnswers["id-d"] === 'Lebron') {
+
+    setTimeout(function () {
+        answerOneA.style.backgroundColor = 'green';
+    }, 1000)
+
+    tryAgain();
+}
+
+
+function answerFour() {
+    if (questionOneAnswers['id-d'] === '72') {
+        answerOneA.removeEventListener('click', answerOne);
+        answerOneB.removeEventListener('click', answerTwo);
+        answerOneC.removeEventListener('click', answerThree);
+        removetheAnswer.removeEventListener('click', removeAnswer);
         answerOneD.style.background = 'red';
-        answerOneC.style.background = 'red';
-        answerOneB.style.background = 'red';
-        setInterval(() => {
-            answerOneA.style.background = 'green';
-
-        }, 1500);
+        choiceD.style.color = 'white';
     }
-})
+    setTimeout(function () {
+        answerOneA.style.backgroundColor = 'green';
+    }, 1000)
+
+    tryAgain();
+}
+
+function removeAnswer() {
+    removetheAnswer.disabled = true;
+    answerB.textContent = '';
+    answerD.textContent = '';
+    answerOneB.removeEventListener('click', answerTwo);
+    answerOneD.removeEventListener('click', answerFour);
+    removetheAnswer.style.opacity = '0.5';
+}
+
+function tryAgain() {
+    answerOneA.removeEventListener('click', answerOne);
+    answerOneD.removeEventListener('click', answerFour);
+    answerOneC.removeEventListener('click', answerThree);
+    answerOneB.removeEventListener('click', answerTwo);
+    let button = document.createElement('input');
+    button.type = 'button';
+    button.value = 'Try Again?';
+    button.setAttribute('id', 'tryAgain');
+    document.getElementById('theCash').appendChild(button);
+    button.onclick = () => {
+        location.href = '../../../../../questions/First/firstQuestion.html';
+
+
+    }
+
+}
---
+++ questions/Second/second.js
@@ -0,0 +1,136 @@
+const questionTwoAnswers = {
+    'id-a': 'ecma2016',
+    'id-b': 'ecma2017',
+    'id-c': 'ecma2015',
+    'id-d': 'ecma2013'
+};
+
+// The Constant variables
+const answerOneA = document.getElementById('question-two-id-a');
+const answerOneB = document.getElementById('question-two-id-b');
+const answerOneC = document.getElementById('question-two-id-c');
+const answerOneD = document.getElementById('question-two-id-d');
+const showcash = document.getElementById('theCash');
+const sideMoney = document.getElementById('secondSum');
+const choiceA = document.querySelector('.choice-a');
+const choiceB = document.querySelector('.choice-b');
+const choiceC = document.querySelector('.choice-c');
+const choiceD = document.querySelector('.choice-d');
+const answerA = document.getElementById('answer-a')
+const answerB = document.getElementById('answer-b')
+const answerC = document.getElementById('answer-c')
+const answerD = document.getElementById('answer-d')
+const removetheAnswer = document.getElementById('removeAnswer');
+let removedAnswer = true;
+
+
+
+
+// The Event Listeners of the variables
+answerOneA.addEventListener('click', answerOne);
+answerOneB.addEventListener('click', answerTwo);
+answerOneC.addEventListener('click', answerThree);
+answerOneD.addEventListener('click', answerFour);
+removetheAnswer.addEventListener('click', removeAnswer);
+
+
+// All the  functions
+function answerThree() {
+
+    if (questionTwoAnswers["id-c"] === 'ecma2015') {
+        answerOneB.removeEventListener('click', answerTwo);
+        answerOneA.removeEventListener('click', answerOne);
+        answerOneD.removeEventListener('click', answerFour);
+        answerOneC.style.background = 'green';
+        sideMoney.setAttribute('id', 'sidemoney');
+    }
+    setTimeout(function () {
+        showcash.setAttribute('id', 'showingMoney');
+        showcash.innerHTML = '200$';
+
+    });
+
+    setTimeout(function () {
+        location.href = '../third/ThirdQuestion.html';
+    }, 2000);
+};
+
+
+
+function answerTwo() {
+
+    if (questionTwoAnswers["id-b"] === 'ecma2017') {
+        answerOneA.removeEventListener('click', answerOne);
+        answerOneC.removeEventListener('click', answerThree);
+        answerOneD.removeEventListener('click', answerFour);
+        answerOneB.style.background = 'red';
+        choiceB.style.color = 'white';
+    }
+
+    setTimeout(function () {
+        answerOneC.style.backgroundColor = 'green';
+    }, 1000)
+
+    tryAgain()
+}
+
+function answerOne() {
+    if (questionTwoAnswers["id-a"] === 'ecma2016') {
+        answerOneC.removeEventListener('click', answerThree);
+        answerOneB.removeEventListener('click', answerTwo);
+        answerOneD.removeEventListener('click', answerFour);
+        answerOneA.style.background = 'red';
+        choiceA.style.color = 'white';
+    }
+
+    setTimeout(function () {
+        answerOneC.style.backgroundColor = 'green';
+    }, 1000)
+
+    tryAgain()
+}
+
+
+function answerFour() {
+    if(questionTwoAnswers['id-d'] === 'ecma2013') {
+        answerOneA.removeEventListener('click', answerOne);
+        answerOneB.removeEventListener('click', answerTwo);
+        answerOneC.removeEventListener('click', answerThree);
+        answerOneD.style.background = 'red';
+        choiceD.style.color = 'white';
+    }
+    setTimeout(function () {
+        answerOneC.style.backgroundColor = 'green';
+    }, 1000)
+
+    tryAgain()
+}
+
+// The Joker of Remove Answers
+function removeAnswer() {
+    answerB.textContent = '';
+    answerA.textContent = '';
+    answerOneB.removeEventListener('click', answerTwo);
+    answerOneA.removeEventListener('click', answerOne);
+    removetheAnswer.style.opacity = '0.5';
+    removetheAnswer.disabled = true;
+}
+
+// Creating button of Trying Again
+function tryAgain() {
+    answerOneA.removeEventListener('click', answerOne);
+    answerOneD.removeEventListener('click', answerFour);
+    answerOneC.removeEventListener('click', answerThree);
+    answerOneB.removeEventListener('click', answerTwo);
+    let button = document.createElement('input');
+    button.type = 'button';
+    button.value = 'Try Again?';
+    button.setAttribute('id', 'tryAgain');
+    document.getElementById('theCash').appendChild(button);
+    button.onclick = () => {
+        location.href = '../../../../../questions/First/firstQuestion.html';
+
+
+    }
+
+}
